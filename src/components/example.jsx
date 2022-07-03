5ae493eea3fbf07c976198914425d5fb68fbe2cf
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-03 09:27:52 +0300

    1

---
+++ src/components/example.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+
+export default function Example(prop) {
+    return <div>
+        <h1>Hello World</h1>
+        <p>its mi</p>
+</div>
+
+}
+
+// console.log(jsxElement)
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// import reportWebVitals from './reportWebVitals';
+// const element = React.createElement('div', { a: 5, b: 10, children: 'Hello' });
+// console.log(element)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +12,13 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+// const jsxElement = <div>Hello</div>;
+// console.log(jsxElement)
+// ReactDOM.render(element, document.querySelector('#root'));
+// ReactDOM.render(element, document.querySelector('#root'))
