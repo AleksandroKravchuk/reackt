f3ba4a57a103fdb7057e451c6cb5b6881a1bff8c
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-21 18:14:10 +0300

    1

--- src/components/example.jsx
+++ src/components/example.jsx
@@ -1,11 +1,10 @@
 // import PropTypes from 'prop-types';
 
 export default function Example(prop) {
-    return <div>
-        <h1>Hello World</h1>
-        <p>its mi</p>
-</div>
-
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>its mi</p>
+    </div>
+  );
 }
-
-// console.log(jsxElement)
